1cc712b0f78fd7769d04f0efd8786ce258978591
Xiann <ns45665412@gmail.com>
2025-07-12 19:53:48 +0800

    bup

--- Models/ManagementModels/ZhongXian/BookQuery/BookQueryDTO.cs
+++ Models/ManagementModels/ZhongXian/BookQuery/BookQueryDTO.cs
@@ -2,23 +2,14 @@
 {
     public class BookQueryDTO
     {
-        //id
         public int collectionId { get; set; }
-        // ISBn
         public string? isbn { get; set; }
-        //封面
         public byte[] collectionImg { get; set; } = null!;
-        //書名
         public string title { get; set; } = null!;
-        // 作者
         public string author { get; set; } = null!;
-        // 簡介
         public string? collectionDesc { get; set; }
-        // 出版社
         public string publisher { get; set; } = null!;
-        // 出版年份
         public DateTime publishDate { get; set; }
-        //  藏書量
         public int NumberOfBook { get; set; }
     }
 }
